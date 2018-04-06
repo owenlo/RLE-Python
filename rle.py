d7e1b8a1d58023382fc7873843218abc74c0459c
owenlo <owenlo@gmail.com>
2018-04-06 15:36:42 +0100

    Decoder added

--- rle.py
+++ rle.py
@@ -1,15 +1,15 @@
-import sys
-
 def main():    
-    rle = "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW"
+    rle = "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW"  
     encoded = encode(rle)  
+    decoded = decode(encoded)
 
-    print('RLE Vector: ' + rle)
-    print('Encoded Result: ' + encoded)  
+    print('Test Vector: ' + rle)
+    print('Encoded Result: ' + encoded)  #Expected output: 12WB12W3B24WB14W
+    print('Decoded Result: ' + decoded)  #Expected output: WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW
 
-def encode(input):
+def encode(input):    
     count = 1
-    result = ''
+    result = ""
     
     for x in range(len(input)):
         if (x == 0):           
@@ -24,11 +24,27 @@ def encode(input):
             count = 1
     
     if(count == 1):
-        result += input[len(input)-1]
+        result += input[len(input) - 1]
     else:
-        result += str(count) + input[len(input)-1]
+        result += str(count) + input[len(input) - 1]
 
-    return result
+    return(result)
+
+def decode(input):
+    count = ""
+    result = ""
+
+    for x in range(len(input)):
+        if input[x].isdigit():
+            count += input[x]
+        else:
+            if(count != ""):
+                result += input[x] * int(count)
+            else:
+                result += input[x]
+            count = ""
+
+    return(result)
 
 if __name__ == '__main__':
     main()
