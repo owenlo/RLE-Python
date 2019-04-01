0d1aac75cedec09d0bc5264f0c674ff35c804703
owenlo <owenlo@gmail.com>
2019-04-01 16:20:32 +0100

    updated to support encoding/decoding of digits

--- rle.py
+++ rle.py
@@ -10,62 +10,65 @@ def main():
     print("Test Vector: " + rle)
 
     # Expected output: 12WB12W3B24WB14W
-    print("Encoded Result: " + encoded)
+    print("Encoded Result: " + formatOutput(encoded))
 
     # Expected output: WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW
     print("Decoded Result: " + decoded)
 
 
 def encode(sequence):
-    """Encode a sequence of characters.
+    """Encode a sequence of characters and return the result as a list of tuples (data value and number of observed instances of value).
 
     Keyword arguments:
-    sequence -- the sequence of characters to encode.
+    sequence -- a sequence of characters to encode represented as a string.
     """
     count = 1
     result = []
 
-    for x,item in enumerate(sequence):
+    for x,item in enumerate(sequence): 
         if x == 0:
             continue
         elif item == sequence[x - 1]:
             count += 1
-        else:
-            if count == 1:
-                result.append(sequence[x - 1])
-            else:
-                result.append(str(count) + sequence[x - 1])
-            count = 1
+        else:        
+            result.append((sequence[x - 1], count))
+            count = 1            
+    
+    result.append((sequence[len(sequence) - 1], count))
 
-    if count == 1:
-        result.append(sequence[len(sequence) - 1])
-    else:
-        result.append(str(count) + sequence[len(sequence) - 1])
-
-    return "".join(result)
+    return result
 
 
 def decode(sequence):
-    """Decode a sequence of characters.
+    """Decodes the sequence and returns the result as a string.
 
-       Keyword arguments:
-       sequence -- the sequence of characters to decode.
-       """
-    count = ""
+    Keyword arguments:
+    sequence -- a list of tuples (data value and number of observed instances of value).
+    """
     result = []
 
-    for x,item in enumerate(sequence):
-        if item.isdigit():
-            count += item
-        else:
-            if count != "":
-                result.append(item * int(count))
-            else:
-                result.append(item)
-            count = ""
+    for item in sequence:
+        result.append(item[0] * item[1])
 
     return "".join(result)
 
+
+def formatOutput(sequence):
+    """Returns a print friendly version of the encoded data. 
+
+    Keyword arguments:
+    sequence -- list of tuples (data value and number of observed instances of value).
+    """
+    result = []
+
+    for item in sequence:
+        if (item[1] == 1):
+            result.append(item[0])
+        else:
+            result.append(str(item[1]) + item[0])
+
+    return "".join(result)
+    
 
 if __name__ == "__main__":
     main()
